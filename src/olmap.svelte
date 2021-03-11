244bf7020e80af4dc71393e7e3bee52a6a70dc3d
Brian H Wilson <bwilson@co.clatsop.or.us>
2021-03-11 12:30:04 -0800

    both maps now use the esri streets map layer

--- src/olmap.svelte
+++ src/olmap.svelte
@@ -1,33 +1,28 @@
 <script>
     import Map from 'ol/Map'
     import View from 'ol/View'
-    import OSM from 'ol/source/OSM'
     import TileLayer from 'ol/layer/Tile'
-    import { fromLonLat, transform } from 'ol/proj'
+    import XYZ from 'ol/source/XYZ'
+    import { fromLonLat } from 'ol/proj'
+
+    const url = 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}'
 
     //props 
     export let center;
     export let zoom;
-    export let options = {};
-
-    const projection = options.projection || 'EPSG:3857';
-
+    
     let viewDiv = "openlayersmap";
     let map = null;
 
     const setupMap = (node, _id) => {
-        const osmLayer = new TileLayer({
-            source: new OSM({
-                // hide attributions from showing up in the middle of the map
-                attributions: ''
-            })
+        const arcgisLayer = new TileLayer({
+            source: new XYZ({ url: url })
         })
         map = new Map({
             target: _id,
-            layers: [osmLayer],
+            layers: [arcgisLayer],
             view: new View({ 
-                // if projection is not latlon we must convert
-                center: projection === 'EPSG:4326' ? center : fromLonLat(center),
+                center: fromLonLat(center),
                 zoom
             })
         });
@@ -43,11 +38,6 @@
 </script>
 
 <div>
-<h1>openlayers and mapbox</h1>
-<div id={viewDiv} class="map" use:setupMap={viewDiv}/>
+    <h1>OpenLayers</h1>
+    <div id={viewDiv} class="map" use:setupMap={viewDiv}/>
 </div>
-
-<style>
-</style>
-
-
