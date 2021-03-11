f7521d43518e5ac2b9c70948a5867f70d2adc09b
Brian H Wilson <bwilson@co.clatsop.or.us>
2021-03-11 12:30:33 -0800

    added footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-	import EsriMap from './esrimap.svelte'
+	import ArcGISMap from './arcgismap.svelte'
 	import OpenlayersMap from './olmap.svelte'
 	const astoria = [-123.8,46.2];
 	const zoom = 12;
@@ -8,22 +8,28 @@
 <main>
 	<div class="mapcontainer">
 		<OpenlayersMap center={astoria} zoom={zoom}/>
-		<EsriMap center={astoria} zoom={zoom} /> 
+		<ArcGISMap center={astoria} zoom={zoom} /> 
 	</div>
 
-	<p>This is a sample app built with the Svelte framework 
-		to experiment with using OpenLayers and 
-		Esri Javascript API to create similar maps. 
-	</p>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-		 to learn how to build Svelte apps.
-	</p>
+	<div class="footer">
+		<p>This demo was built with the <a href="https://svelte.dev">Svelte framework</a> 
+			to experiment with <a href="https:openlayers.org">OpenLayers</a> and 
+			<a href="https://developers.arcgis.com/javascript/latest/">Esri ArcGIS API</a>
+			to create similar maps, side-by-side.
+			<i>I want to tie the extent of the two maps together
+			but that's not what I am working on today.</i> 
+		</p>
+		<p>Total lines of code in this project: 131, ol map: 43, esri map: 41</p>
+	</div>
 </main>
 
 <style>
 	main {
 		padding: 1em;
 		margin: 0 auto;
+	}
+	.footer {
+		width: 800px;
 	}
 	.mapcontainer {
 		display: flex;
